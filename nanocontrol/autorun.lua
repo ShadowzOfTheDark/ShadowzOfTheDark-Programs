bca7edb5c5dc4deca12d08ac7b1668a7247ce10f
ShadowzOfTheDark <shadowzofthedark@gmail.com>
2024-07-19 19:55:52 -0500

    Delete autorun.lua

--- nanocontrol/autorun.lua
+++
@@ -1,8 +0,0 @@
--- NanoControl
--- A program with GUI and commands to better control your nanomachines.
--- https://github.com/ShadowzOfTheDark/ShadowzOfTheDark-Programs/nanomachines+
-
--- This just exists to make the nanocontrol "nc" shorthand command function.
-
-shell = require("shell")
-shell.setAlias("nc","nanocontrol")
